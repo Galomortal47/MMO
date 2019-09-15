afd7a516e5380a399e69a45f80183edd670a5946
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-09-14 22:34:40 -0300

    updates

--- Godot/Network.gd
+++ Godot/Network.gd
@@ -3,11 +3,17 @@ extends Node
 var packet = StreamPeerTCP.new()
 var i = 0
 func _ready():
-	packet.connect_to_host( "34.69.136.132", 8082 )
+	packet.connect_to_host( "::1", 8082 )
+
+var json = {
+	"lv":3,
+	"hp":4,
+	"pos_x":0,
+	"pos_y":0
+}
 
 func _process(delta):
-	packet.put_string(str(i))
-	i+=1
+	packet.put_string(str(json))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
---
+++ client.js
@@ -0,0 +1,31 @@
+var now = require('performance-now');
+var _ = require('underscore');
+require('./packet.js')
+
+module.exports = function(){
+	
+	//these objects will be added at runtime....
+	//this.socket = {}
+	//this.user = {}
+	
+	this.initiate = function(){
+		var client = this;
+		//send the conection hand shake to the client
+		var packet = 'hello';
+		console.log("packet was send " + packet);
+		client.socket.write(packet)
+		
+		console.log('client initiated')
+		//do stuff
+	}
+	
+	this.data = function(data){
+		console.log("client connectted " + data.toString()); 
+	}
+	this.error = function(err){
+		console.log("client connectted " + err.toString());
+	}
+	this.end = function(){
+		console.log("client closed"); 
+	}
+	}
